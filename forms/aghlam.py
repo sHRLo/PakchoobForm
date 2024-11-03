4b2ce6df45adbfe292b3e586b734aadc619a6d08
sHRLo <62970136+sHRLo@users.noreply.github.com>
2024-11-03 23:28:36 +0330

    Add files via upload

---
+++ forms/aghlam.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pyodbc
+
+
+
+def aghlam_form():
+    with st.form('aghlam_form'):
+        form_code = st.text_input('کد درخواست')
+        form_date = st.date_input('تاریخ')
+        form_discription = st.text_input('شرح قلم')
+        form_numb = st.text_input('تعداد قلم')
+        form_unit = st.selectbox('نوع قلم', options=['عدد','متر','سانتی متر','میلی متر','گرم','کیلوگرم','لیتر'])
+        submit_button = st.form_submit_button('ثبت اقلام')
+        
+        if submit_button:
+            st.success('Nice')
